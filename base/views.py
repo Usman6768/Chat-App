cb5e2a6d2daaebf038595b6bcc9eee1770bd892c
Usman Pervez <152008530+Usman6768@users.noreply.github.com>
2024-07-23 13:30:31 +0500

    Added Search Functionality

--- base/views.py
+++ base/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(Q(topic__name__icontains= q) | Q(name__icontains=q) | Q(description__icontains=q))
+
+    topics = Topic.objects.all()
+    room_count = rooms.count() 
+     
+
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -35,3 +42,11 @@ def updateRoom(request, pk):
             return redirect('home')
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
