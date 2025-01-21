e60d689d46638fc8239b1fb7025111ebc9f2c142
Usman Pervez <usmanpervez5@gmail.com>
2025-01-21 18:53:32 +0500

    Complete Auth UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
 
 function App() {
 
 
   return (
-    <>
-      <Button>Click Me</Button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth' element={<Auth/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+
+        <Route path="*" element={<Navigate to="/auth"/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
